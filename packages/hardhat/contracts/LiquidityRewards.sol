77fbc0eeff7e27708bce5473a0aef8e7218c0a51
Derek Moon <lxnkzr@gmail.com>
2021-11-04 14:57:51 -0400

    Added contract to test file

--- packages/hardhat/contracts/LiquidityRewards.sol
+++ packages/hardhat/contracts/LiquidityRewards.sol
@@ -3,16 +3,14 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-// import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract LiquidityRewards is Ownable {
   using SafeMath for uint256;
-  // using SafeERC20 for IERC20;
+  using SafeERC20 for IERC20;
 
   struct UserInfo {
     uint256 oldestActiveDepositBlock;
@@ -24,7 +22,7 @@ contract LiquidityRewards is Ownable {
   address public oneMonthAddr; // nft reward after 1 months of liquidity
   uint16 public tokenAmountRequired; // minumum amount required to deposit be eligible to mint nft
   uint16 public burnReward; // reward for burning NFT
-  IERC20 public lpToken;
+  IERC20 public lpToken; 
 
 
   event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -63,7 +61,7 @@ contract LiquidityRewards is Ownable {
       user.oldestActiveDepositBlock = block.timestamp;
     }
 
-    // lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+    lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
     user.amount = user.amount.add(_amount);
     emit Deposit(msg.sender, _pid, _amount);
   }
@@ -74,7 +72,7 @@ contract LiquidityRewards is Ownable {
     require(user.amount >= _amount, "withdraw: not good");
 
     user.amount = user.amount.sub(_amount);
-    // lpToken.safeTransferFrom(address(this), address(msg.sender), _amount);
+    lpToken.safeTransferFrom(address(this), address(msg.sender), _amount);
 
     if(tokenAmountRequired > user.amount){  
        user.oldestActiveDepositBlock = 0;
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -4,42 +4,44 @@ const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe("My Dapp", function () {
+describe("LiquidityRewards", function () {
   let myContract;
 
-  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
   before((done) => {
     setTimeout(done, 2000);
   });
 
   describe("LiquidityRewards", function () {
-    it("Should deploy YourContract", async function () {
+    it("Should deploy LiquidityRewards", async function () {
+      const OneMonthNFT = await ethers.getContractFactory("OneMonthNFT");
+      const oneMonthNFT = await OneMonthNFT.deploy();
+
       const LiquidityRewards = await ethers.getContractFactory(
         "LiquidityRewards"
       );
 
-      myContract = await LiquidityRewards.deploy();
+      myContract = await LiquidityRewards.deploy(oneMonthNFT.address, 50, 50);
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+    // describe("setPurpose()", function () {
+    //   it("Should be able to set a new purpose", async function () {
+    //     const newPurpose = "Test Purpose";
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
+    //     await myContract.setPurpose(newPurpose);
+    //     expect(await myContract.purpose()).to.equal(newPurpose);
+    //   });
 
-      // Uncomment the event and emit lines in YourContract.sol to make this test pass
+    //   // Uncomment the event and emit lines in YourContract.sol to make this test pass
 
-      it("Should emit a SetPurpose event ", async function () {
-        const [owner] = await ethers.getSigners();
+    //   it("Should emit a SetPurpose event ", async function () {
+    //     const [owner] = await ethers.getSigners();
 
-        const newPurpose = "Another Test Purpose";
+    //     const newPurpose = "Another Test Purpose";
 
-        expect(await myContract.setPurpose(newPurpose))
-          .to.emit(myContract, "SetPurpose")
-          .withArgs(owner.address, newPurpose);
-      });
-    });
+    //     expect(await myContract.setPurpose(newPurpose))
+    //       .to.emit(myContract, "SetPurpose")
+    //       .withArgs(owner.address, newPurpose);
+    //   });
+    // });
   });
 });
