1c591c05f2e77e60fb023529bec0023b87cee7fa
Derek Moon <lxnkzr@gmail.com>
2021-11-03 21:59:28 -0400

    Added requirements to mintOneMonthNFT and IERC20 addition/ subtraction

--- packages/hardhat/contracts/LiquidityRewards.sol
+++ packages/hardhat/contracts/LiquidityRewards.sol
@@ -1,12 +1,12 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 // import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-// import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
@@ -14,7 +14,7 @@ contract LiquidityRewards is Ownable {
 
 
   struct UserInfo {
-    uint oldestActiveDepositDate;
+    uint256 oldestActiveDepositBlock;
     uint amount;
     // uint listPointer. not needed
   }
@@ -27,7 +27,9 @@ contract LiquidityRewards is Ownable {
   }
 
 
-  mapping (address => UserInfo) public userInfo; // liquidityToOwner[msg.sender] = ProviderInfo.id
+  // mapping (address => UserInfo) public userInfo; // liquidityToOwner[msg.sender] = ProviderInfo.id
+  mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+
   PoolInfo[] public poolInfo;
 
 
@@ -37,6 +39,8 @@ contract LiquidityRewards is Ownable {
   address public oneMonthAddr; // nft reward after 1 months of liquidity
   address public twoMonthAddr; // nft reward after 2 months of liquidity
   address public sixMonthAddr; // nft reward after 6 months of liquidity
+
+  uint16 public lowerAmountBoundary; // minumum amount required to deposit for tier 1
 
   IERC20 public lpToken;
   uint16 public burnReward; // reward for burning NFT
@@ -77,6 +81,10 @@ contract LiquidityRewards is Ownable {
 
   }
 
+  using SafeMath for uint256;
+
+
+
   function setPurpose(string memory _newPurpose) public {
     purpose = _newPurpose;
     console.log(msg.sender,"set purpose to",purpose);
@@ -93,33 +101,55 @@ contract LiquidityRewards is Ownable {
     if (user.amount > 0) {
         // uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
         // safeSushiTransfer(msg.sender, pending);
-
+        user.amount = user.amount.add(_amount);
     }
-    pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+
+    if (user.amount == 0) {
+         user.oldestActiveDepositBlock = block.timestamp;
+    }
+
+    // pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
     user.amount = user.amount.add(_amount);
     emit Deposit(msg.sender, _pid, _amount);
   }
 
    // Withdraw LP tokens from MasterChef.
   function withdraw(uint256 _pid, uint256 _amount) public {
-    PoolInfo storage pool = poolInfo[_pid];
+    // PoolInfo storage pool = poolInfo[_pid];
     UserInfo storage user = userInfo[_pid][msg.sender];
     require(user.amount >= _amount, "withdraw: not good");
     // updatePool(_pid);
-    uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
+
+    // check if transaction is pending for this scope?
+    // uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
 
     // safeSushiTransfer(msg.sender, pending);
     user.amount = user.amount.sub(_amount);
-    user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
-    pool.lpToken.safeTransfer(address(msg.sender), _amount);
+    // user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
+
+    if(lowerAmountBoundary > user.amount){  
+       user.oldestActiveDepositBlock = 0;
+    }
+
+    // pool.lpToken.safeTransfer(address(msg.sender), _amount);
     emit Withdraw(msg.sender, _pid, _amount);
     }
 
-  function mintOneMonthNFT() public {
+  function mintOneMonthNFT(uint256 _pid) public {
+    UserInfo storage user = userInfo[_pid][msg.sender];
+
+    require(lowerAmountBoundary < user.amount, "Not enough tokens");
+
     // require() // find liquidity on uniswap 
     // require() // liquidity is found in mapping
+    
+    // uint amountSince = block.timestamp - user.oldestActiveDepositBlock
+    
+    require(block.timestamp < user.oldestActiveDepositBlock + 30 days, "Not enough time has passed"); //https://ethereum.stackexchange.com/questions/5924/how-do-ethereum-mining-nodes-maintain-a-time-consistent-with-the-network/5931#5931
+
     // require() // time requirement (user deposited to this contract more than (1/2/6 months ago))
-    // tokenId = NFTContract.mint(msg.sender)
+
+    // mint() NFT
     // emit OneMonthNFTMinted(msg.sender, msg.sender); 
   }
 
